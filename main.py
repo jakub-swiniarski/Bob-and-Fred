8d1eb31790f4479792f4077517241e6c8e159b9c
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-02-15 13:25:21 +0100

    idk

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ class Player(pygame.sprite.Sprite):
         self.counter_skin=0
         self.counter_jump=0
 
+        self.pressed_keys = {"left": False, "right": False}
+        self.fire=False
+
         #walking sprites
         self.img_r_w1 = pygame.image.load("img/bob/bob_r_w1.png").convert_alpha()
         self.img_r_w1 = pygame.transform.scale(self.img_r_w1, (100, 130))
@@ -70,6 +73,25 @@ class Player(pygame.sprite.Sprite):
         else:
             self.rect.x -= 500 * dt
 
+class Gun(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.img_r=pygame.image.load("img/items/ak47_r.png").convert_alpha()
+        self.img_r=pygame.transform.scale(self.img_r,(120,40))
+
+        self.img_l = pygame.image.load("img/items/ak47_l.png").convert_alpha()
+        self.img_l = pygame.transform.scale(self.img_l, (120, 40))
+
+        self.image = self.img_r
+        self.rect = self.image.get_rect()
+        self.counter=0
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.img=pygame.image.load("img/items/bullet.png").convert_alpha()
+        self.img=pygame.transform.scale(self.img,(20,20))
+
 sprite_list = pygame.sprite.Group()
 
 bob=Player()
@@ -84,7 +106,11 @@ rect_floor = image_floor.get_rect()
 rect_floor.x=0
 rect_floor.y=520
 
-pressed_keys_bob = {"left": False, "right": False}
+#items
+ak47=Gun()
+ak47.rect.x=100
+ak47.rect.y=470
+sprite_list.add(ak47)
 
 while True:
     for event in pygame.event.get():
@@ -94,20 +120,24 @@ while True:
         elif event.type == pygame.KEYDOWN:
             #bob
             if event.key == pygame.K_a:
-                pressed_keys_bob["left"] = True
+                bob.pressed_keys["left"] = True
             if event.key == pygame.K_d:
-                pressed_keys_bob["right"] = True
+                bob.pressed_keys["right"] = True
             if event.key == pygame.K_SPACE:
                 if bob.rect.y > 389:
                     bob.jumping = True
                     bob.counter_jump=0
+            if event.key == pygame.K_f:
+                bob.fire=True
 
         elif event.type == pygame.KEYUP:
             #bob
             if event.key == pygame.K_a:
-                pressed_keys_bob["left"] = False
+                bob.pressed_keys["left"] = False
             if event.key == pygame.K_d:
-                pressed_keys_bob["right"] = False
+                bob.pressed_keys["right"] = False
+            if event.key == pygame.K_f:
+                bob.fire=False
 
     bob.walking=False
 
@@ -116,16 +146,22 @@ while True:
     dt = now - prev_time
     prev_time = now
 
-    if pressed_keys_bob["left"] and pressed_keys_bob["right"]:
+    if bob.pressed_keys["left"] and bob.pressed_keys["right"]:
         bob.walking=False
     else:
-        if pressed_keys_bob["left"]:
+        if bob.pressed_keys["left"]:
             bob.right = False
             bob.walk()
 
-        if pressed_keys_bob["right"]:
+        if bob.pressed_keys["right"]:
             bob.right = True
             bob.walk()
+
+    #bob gun
+    if bob.fire:
+        ak47.counter += 250
+        if ak47.counter % 1250 == 0:
+            print("i fire")
 
     if bob.walking==False:
         if bob.right:
@@ -154,6 +190,14 @@ while True:
         bob.rect.x-=500*dt
 
     #add a 2nd player controlled by wsad, 1v1 game
+
+    ak47.rect.y=bob.rect.y+75
+    if bob.right:
+        ak47.image=ak47.img_r
+        ak47.rect.x = bob.rect.x
+    else:
+        ak47.image=ak47.img_l
+        ak47.rect.x = bob.rect.x-20
 
     clock.tick(75)
     screen.fill(bg)
